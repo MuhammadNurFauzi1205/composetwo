f94e84d39311889be03d695c2eb5dc849eab0ca5
MuhammadNurFauzi1205 <Mayor.title@gmail.com>
2023-10-12 15:09:44 +0700

    greeting card
    mencoba membuat greeting card happy birthday samatha from emma sesuai code lab dengan image dan text

--- app/src/main/java/com/example/composetwo/MainActivity.kt
+++ app/src/main/java/com/example/composetwo/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.composetwo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composetwo.ui.theme.ComposeTwoTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +29,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTwoTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    GreetingImage(getString(R.string.happy_birthday_text), getString(R.string.signature_text))
                 }
             }
         }
@@ -30,17 +38,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    // Create a column so that texts don't overlap
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    //Step 3 create a box to overlap image and texts
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun BirthdayCardPreview() {
     ComposeTwoTheme {
-        Greeting("Android")
+        GreetingImage("Happy Birthday Sam!", "From Emma")
     }
 }
