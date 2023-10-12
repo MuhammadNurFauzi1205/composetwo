0ae26dda27f453403294f07baccdbbb07defc8d8
MuhammadNurFauzi1205 <Mayor.title@gmail.com>
2023-10-12 20:33:41 +0700

    compose article
    membuat compose article

--- app/src/main/java/com/example/composetwo/MainActivity.kt
+++ app/src/main/java/com/example/composetwo/MainActivity.kt
@@ -15,8 +15,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,60 +31,103 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTwoTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    GreetingImage(getString(R.string.happy_birthday_text), getString(R.string.signature_text))
+                Surface(
+                    color = MaterialTheme.colorScheme.background,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    ComposeArticleApp()
                 }
             }
         }
     }
 }
 
+//@Composable
+//fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+//    // Create a column so that texts don't overlap
+//    Column(
+//        verticalArrangement = Arrangement.Center,
+//        modifier = modifier
+//    ) {
+//        Text(
+//            text = message,
+//            fontSize = 24.sp,
+//            lineHeight = 116.sp,
+//            textAlign = TextAlign.Center
+//
+//        )
+//        Text(
+//            text = from,
+//            fontSize = 36.sp,
+//            modifier = Modifier
+//                .padding(16.dp)
+//                .align(alignment = Alignment.CenterHorizontally)
+//        )
+//    }
+//}
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+fun ArticleText(title: String, subtitle: String, content: String, imagePainter: Painter, modifier: Modifier = Modifier) {
     // Create a column so that texts don't overlap
     Column(
-        verticalArrangement = Arrangement.Center,
         modifier = modifier
     ) {
+        Image(painter = imagePainter, contentDescription = null)
         Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
-
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
+            text = title,
+            fontSize = 24.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.CenterHorizontally)
+
+        )
+        Text(
+            text = subtitle,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+
+        )
+        Text(
+            text = content,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(16.dp)
+
         )
     }
 }
+
+//@Composable
+//fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+//    val image = painterResource(R.drawable.bg_compose_background)
+//    //Step 3 create a box to overlap image and texts
+//    Box {
+//        Image(
+//            painter = image,
+//            contentDescription = null,
+//            contentScale = ContentScale.Crop,
+//            alpha = 0.5F
+//        )
+//        GreetingText(
+//            message = message,
+//            from = from
+//        )
+//    }
+//}
 
 @Composable
-fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.androidparty)
-    //Step 3 create a box to overlap image and texts
-    Box {
-        Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
-        )
-        GreetingText(
-            message = message,
-            from = from
-        )
-    }
+fun ComposeArticleApp() {
+    ArticleText(
+        title = stringResource(R.string.title),
+        subtitle = stringResource(R.string.subtitle),
+        content = stringResource(R.string.content),
+        imagePainter = painterResource(R.drawable.bg_compose_background)
+    )
 }
 
-@Preview(showBackground = false)
+@Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     ComposeTwoTheme {
-        GreetingImage("Happy Birthday Sam!", "From Emma")
+        ComposeArticleApp()
     }
 }
